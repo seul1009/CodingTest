320cbd586177273aee6f334d39be38cc133fb975
seuli <tmfdl8414@naver.com>
2024-11-11 21:04:02 +0900

    [Gold V] Title: 평범한 배낭, Time: 3032 ms, Memory: 34972 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+dp = [0] * (K + 1)
+
+for _ in range(N):
+    w, v = map(int, input().split())
+    for i in range(K, w - 1, -1):
+        dp[i] = max(dp[i], dp[i - w] + v)
+
+print(dp[K])

a8dd7c6fd427332f4f99c39456bcda5ecbb8267d
SEULI <tmfdl8414@naver.com>
2025-04-04 17:07:53 +0900

    [Silver I] Title: 정수 삼각형, Time: 124 ms, Memory: 41524 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,12 +1,16 @@
 n = int(input())
 triangle = [list(map(int, input().split())) for _ in range(n)]
 
+DP = [[0] * (i + 1) for i in range(n)]
+DP[0][0] = triangle[0][0]
+
 for i in range(1, n):
     for j in range(i + 1):
-        left = triangle[i-1][j-1] if j > 0 else 0
-        
-        up = triangle[i-1][j] if j < i else 0
-        
-        triangle[i][j] += max(left, up)
+        if j == 0:
+            DP[i][j] = DP[i-1][j] + triangle[i][j]
+        elif j == i:
+            DP[i][j] = DP[i-1][j-1] + triangle[i][j]
+        else:
+            DP[i][j] = max(DP[i-1][j-1], DP[i-1][j]) + triangle[i][j]
 
-print(max(triangle[n-1]))
+print(max(DP[n-1]))

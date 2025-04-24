7a0d24b77af910b34f97285391900e7d04405e7f
SEULI <tmfdl8414@naver.com>
2025-04-24 20:47:11 +0900

    [Gold IV] Title: 동전 1, Time: 148 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,11 @@
+n, k = map(int, input().split())
+
+coins = [int(input()) for _ in range(n)]
+coins.sort()
+
+DP = [0] * (k + 1)
+DP[0] = 1
+for c in coins:
+    for i in range(c, k + 1):
+        DP[i] += DP[i - c]
+print(DP[k])

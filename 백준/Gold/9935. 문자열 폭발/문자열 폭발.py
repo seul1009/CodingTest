70635a3c7c680be70542a6ae5a06424629b07795
Seuli Gu <tmfdl8414@naver.com>
2025-06-24 17:11:17 +0900

    [Gold IV] Title: 문자열 폭발, Time: 636 ms, Memory: 43596 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -4,17 +4,13 @@ input = sys.stdin.readline
 strings = input().rstrip()
 explosion = input().rstrip()
 e = len(explosion)
-idx = 0
 stack = []
 
-while True:
-    for s in strings:
-        stack.append(s)
-        if "".join(stack[-e:]) == explosion:
-            for _ in range(e):
-                stack.pop()
-    break
-
+for s in strings:
+    stack.append(s)
+    if stack[-e:] == list(explosion):
+        for _ in range(e):
+            stack.pop()
 if stack:
     print("".join(stack))
 else:

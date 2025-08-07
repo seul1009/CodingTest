e40ac7d68811c2c1481954a4cb3344cb6ec3c8c2
Seuli Gu <tmfdl8414@naver.com>
2025-08-07 13:18:37 +0900

    [Silver V] Title: 삽입 정렬을 해볼까, Time: 1128 ms, Memory: 158276 KB -BaekjoonHub

--- 백준/Silver/33846. 삽입 정렬을 해볼까/삽입 정렬을 해볼까.py
+++ 백준/Silver/33846. 삽입 정렬을 해볼까/삽입 정렬을 해볼까.py
@@ -4,5 +4,7 @@ input = sys.stdin.readline
 n, t = map(int, input().split())
 arr = list(map(int, input().split()))
 
-answer = sorted(arr[:t]) + arr[t:]
+answer = sorted(arr[:t])
+answer.extend(arr[t:])  
+
 print(*answer)

92284c3c060f0d1f5cfe83b7ebd4662231f25d18
Seuli <tmfdl8414@naver.com>
2025-10-24 21:19:51 +0900

    [level 2] Title: 카펫, Time: 0.29 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,20 +1,11 @@
 def solution(brown, yellow):
-    # 총 블록 수
-    allBlock = brown + yellow
-
-    # 초기 가로, 세로 길이
-    width = 3
-    height = 1
-
-    while True:
-        # 총 블록수 / 가로길이 --> 몫, 나머지
-        height = allBlock // width
-        remainder = allBlock % width
-
-        # 나머지 0이고 가로 길이 >= 세로 길이 이고
-        # 중앙 블록 (가로 -2 * 세로 - 2) == 노랑 블록 수 이면 해당 가로,세로 길이 값 리턴
-        if remainder == 0 and width >= height and yellow == (width-2) * (height-2):
-            return [width, height]
-
-        # 그게 아니면 가로 길이 +1
-        width += 1
+    total = brown + yellow
+    
+    for h in range(1, total + 1):
+        if total % h == 0:
+            w = total // h
+            if w < h:
+                continue
+        
+        if (w-2) * (h-2) == yellow:
+            return [w, h]

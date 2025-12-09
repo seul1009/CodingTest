a4f506c1f939bfd1e49bee74bd82a3b9982af534
Seuli <tmfdl8414@naver.com>
2025-12-09 16:39:09 +0900

    [Silver I] Title: 동물원, Time: 180 ms, Memory: 52648 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -0,0 +1,15 @@
+N = int(input())
+dp = [[0] * 3 for _ in range(N + 1)]
+
+for i in range(3):
+    dp[1][i] = 1
+
+for i in range(2, N + 1):
+    if dp[i - 1][0]:
+        dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901
+    if dp[i - 1][1]:
+        dp[i][1] = dp[i - 1][0] + dp[i - 1][2] % 9901
+    if dp[i - 1][2]:
+        dp[i][2] = dp[i - 1][0] + dp[i - 1][1] % 9901
+
+print(sum(dp[N]) % 9901)

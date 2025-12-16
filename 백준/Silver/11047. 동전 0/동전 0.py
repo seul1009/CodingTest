6beaad02f1e139a1f20ff1028069d578512def12
Seuli <tmfdl8414@naver.com>
2025-12-16 20:50:52 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+n, k = map(int, input().split())
+
+coin = []
+for i in range(n):
+    coin.append(int(input()))
+
+count = 0
+for i in reversed(range(n)):
+    count += k // coin[i]
+    k %= coin[i]
+print(count)
